08f3737d86a274bf6dbfe81f584ba58bccd13241
Franco Sarachu <fsarachu@gmail.com>
2016-09-26 15:31:28 -0300

    Format code

--- check_profanity.py
+++ check_profanity.py
@@ -8,10 +8,12 @@ def read_text():
     quotes.close()
     return file_contents
 
+
 def check_profanity(text_to_check):
     connection = urllib.urlopen("http://www.purgomalum.com/service/containsprofanity?text=" + text_to_check)
     response = connection.read()
     connection.close()
     print response
+
 
 check_profanity(read_text())
